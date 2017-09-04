26830a4ca6ff7aa99d9736c81f1caf94b20e2281
waltonb7944 <31635667+waltonb7944@users.noreply.github.com>
2017-09-04 19:45:07 -0400

    Add files via upload
    M2HW1 Distance Traveled Assignment

---
+++ M2HW1_DistanceTraveled_Alvarez.py
@@ -0,0 +1,32 @@
+
+# CTI-110
+# M2HW1 - Distanced Traveled
+# Brittani Alvarez
+# 09/04/2017
+
+
+# Assuming no delays, the distance that a car travels down the interstate
+# can be calculated with the formula: Distance = Speed X Time
+
+# Assign the speed of 70 MPH 
+
+speed = 70
+
+# Assign the different hours the car will travel
+
+hours6 = 6
+hours10 = 10
+hours15 = 15
+
+# multiple the speed by how many hours the car traveled
+
+distanceAfter6 = speed * hours6
+distanceAfter10 = speed * hours10
+distanceAfter15 = speed * hours15
+
+# display hour far the car traveled in those hours
+
+print ("Traveling at 70 MPH.")
+print ("After traveling for 6 hours you have traveled", distanceAfter6, "miles.")
+print ("After traveling for 10 hours you have traveled", distanceAfter10, "miles.")
+print ("After traveling for 15 hours you have traveled", distanceAfter15, "miles.")
