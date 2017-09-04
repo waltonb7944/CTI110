cc3ad7756a864bb304b66381689dbea67fc50e5c
waltonb7944 <31635667+waltonb7944@users.noreply.github.com>
2017-09-04 19:00:28 -0400

    Add files via upload
    M2T1_SalesPredictions upload for M2LAB1 assignment

---
+++ M2T1_SalesPrediction_Alvarez.py
@@ -0,0 +1,16 @@
+# M2T1
+# Sales Prediction
+# Brittani Alvarez
+# 9/3/17
+
+
+# Get the projected total sales.
+totalSales = float(input ("Enter the projected sales: "))
+
+# Calculate the profit as 23 percent of total sales.
+annualProfit = totalSales * 0.23
+
+# Display the profit.
+print ('The profit is $', format(annualProfit, ',.2f'))
+                                
+                                 
